17465796b0b14c68f2e032401b4e31a088ee242e
Kevin <kevin.tassi@gmail.com>
2019-12-29 13:00:13 +0100

    init the app

---
+++ app.js
@@ -0,0 +1,5 @@
+console.log("+1", this);
+console.log("+2", window);
+
+console.log("++", _greeting);
+console.log("++", _greeting2);
---
+++ helper.js
@@ -0,0 +1,8 @@
+{
+  var _greeting = function(name) {
+    console.log("Hello" + name);
+  };
+  let _greeting2 = function(name) {
+    console.log("Hello 2 " + name);
+  };
+}
