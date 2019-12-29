14caadbfa79651bca39293602f8782119520cd6a
Kevin <kevin.tassi@gmail.com>
2019-12-29 16:03:08 +0100

    Create first react component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <h1 className="hello">My name is!</h1>,
-  document.getElementById("appContainer")
+const FooterComponent = function() {
+  return <footer>Copyright 2020</footer>;
+};
+
+const app = (
+  <div>
+    <nav>
+      <ul>
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <a href="">Profile</a>
+        </li>
+      </ul>
+    </nav>
+    <main>
+      <p>Dummy text</p>
+    </main>
+    <FooterComponent />
+  </div>
 );
+
+ReactDOM.render(app, document.getElementById("appContainer"));
